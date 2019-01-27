76264ec8f1a62d2119d4103959ea0b76d4a6d306
Oliver Mansfield <omansfield88@gmail.com>
2019-01-27 19:21:09 +0000

    Filters layout on Mens page

--- src/components/Mens.js
+++ src/components/Mens.js
@@ -162,10 +162,12 @@ class Mens extends Component {
       <main className="product-page">
         <Header />
         <h2>Men's</h2>
-        <button onClick={this.removeFilters} className="filter-button">All</button>
-        <button onClick={this.filterShoes} className="filter-button">Shoes</button>
-        <button onClick={this.filterCoat} className="filter-button">Coats</button>
-        <button onClick={this.filterShirts} className="filter-button">Shirts</button>
+        <div className="filters">
+          <button onClick={this.removeFilters} className="filter-button">All</button>
+          <button onClick={this.filterShoes} className="filter-button">Shoes</button>
+          <button onClick={this.filterCoat} className="filter-button">Coats</button>
+          <button onClick={this.filterShirts} className="filter-button">Shirts</button>
+        </div>
         <ul className="list-of-products">
           {listOfProducts}
         </ul>
